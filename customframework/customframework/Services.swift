e89dd60c219d34c39b958c7e0270176d7e90cc27
Augustin A <augustin.a@visionetsystems.com>
2019-07-04 15:49:52 +0530

    finished ourcustomframework

---
+++ customframework/customframework/Services.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  customframework
+//
+//  Created by Anvesh on 04/07/19.
+//  Copyright © 2019 Anvesh. All rights reserved.
+//
+
+import Foundation
+public class services{
+    
+    private init(){}
+    
+    public static func dosomething()-> String {
+        
+        return "i did something"
+    }
+    
+}
---
+++ customframework/customframework/customframework.h
@@ -0,0 +1,19 @@
+//
+//  customframework.h
+//  customframework
+//
+//  Created by Anvesh on 04/07/19.
+//  Copyright © 2019 Anvesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for customframework.
+FOUNDATION_EXPORT double customframeworkVersionNumber;
+
+//! Project version string for customframework.
+FOUNDATION_EXPORT const unsigned char customframeworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <customframework/PublicHeader.h>
+
+
